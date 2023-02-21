634f4207f5292b825b40fd0465db29ab92e8dc70
failer64 <failer64@rambler.ru>
2023-02-21 16:28:46 +0300

    add div

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 
 function App() {
 
-  return (
-    <div className="App">
-      <p>1</p>
-      <p>2</p>
-    </div>
-  )
+    return (
+        <div className="App">
+            <p>1</p>
+            <p>2</p>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam aperiam, architecto autem
+                cupiditate deleniti doloremque enim excepturi fugit, hic magni maxime nostrum quaerat, qui quisquam
+                recusandae repellendus suscipit veritatis?
+            </div>
+        </div>
+    )
 }
 
 export default App
